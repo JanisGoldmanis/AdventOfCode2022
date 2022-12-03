ea123666df49a28040d126ecff9b649ca19f7f03
Janis <janis.goldmanis@gmail.com>
2022-12-03 18:22:16 +0200

    Day3.2

--- Day3.py
+++ Day3.py
@@ -3,3 +3,10 @@ def Day3_first_half():
     file = open('Day3.txt', 'r')
     values = [line.rstrip().replace(' ', '') for line in file]
     print(sum([(string.ascii_lowercase+string.ascii_uppercase).index(char)+1 for line in values for char in set(line[:int(len(line)/2)]) if char in line[int(len(line)/2):]]))
+
+
+def Day3_second_half():
+    file = open('Day3.txt', 'r')
+    values = [line.rstrip().replace(' ', '') for line in file]
+    print(sum([(string.ascii_lowercase+string.ascii_uppercase).index(char)+1 for i in range(int(len(values)/3)) for char in set(values[i*3]) if char in values[i*3+1] and char in values[i*3+2]]))
+
--- main.py
+++ main.py
@@ -6,4 +6,5 @@ import Day3
 # Day1.Day1_second_half()
 # Day2.Day2_first_half()
 # Day2.Day2_second_half()
-Day3.Day3_first_half()
+# Day3.Day3_first_half()
+Day3.Day3_second_half()
